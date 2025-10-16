8b463e8e0b2a4ab3e4e84677fd6217dff1518c96
liplum <Li_plum@outlook.com>
2025-10-17 03:10:29 +0800

    feat: Implement basic `on` method for web
    Begin the implementation of the `on` method in `EventSourceWeb`. This change adds a listener for a given event to the underlying `EventSource` instance.
    
    Key changes include:
    - Renaming the internal `_start` method to `start` and making it public.
    - Storing the `web.EventSource` instance in a private `_sse` field upon starting.
    - Using `_sse.addEventListener` within the `on` method to listen for events, with a placeholder callback function.
    - Adding necessary imports for `dart:async` and `dart:js_interop`.

--- lib/src/web.dart
+++ lib/src/web.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:event_source_plus/src/entity/pack.dart';
 import 'package:web/web.dart' as web;
 import 'event_source.dart';
+import 'dart:js_interop';
 
 class EventSourceWebConfig {
   final String url;
@@ -11,18 +14,21 @@ class EventSourceWebConfig {
 
 class EventSourceWeb implements EventSource {
   final EventSourceWebConfig? webConfig;
+  web.EventSource? _sse;
 
   EventSourceWeb({required this.webConfig});
 
-  Future<void> _start() async {
+  Future<void> start() async {
     final webConfig = this.webConfig;
     if (webConfig == null) return;
     final eventSource = web.EventSource(webConfig.url, web.EventSourceInit(withCredentials: webConfig.withCredentials));
+    _sse = eventSource;
   }
 
   @override
   Stream<EventPack> on(String event) {
-    // TODO: implement on
-    throw UnimplementedError();
+    _sse!.addEventListener(event, (web.MessageEvent e) {
+
+    }.toJS);
   }
 }
